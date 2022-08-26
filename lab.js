917ee4729e82b3164bee34ae7a5592077f4224c3
tayjanks <taylorjenkins801@gmail.com>
2022-08-25 18:31:58 -0600

    problem four attempt two

--- lab.js
+++ lab.js
@@ -101,6 +101,7 @@ let passingScore = 7;
 
 //CODE HERE
 
+
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
