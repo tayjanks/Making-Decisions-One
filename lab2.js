d66bc2e7ec7e067dd1078bdcca8bb7e36fc322f5
tayjanks <taylorjenkins801@gmail.com>
2022-08-25 19:33:43 -0600

    problem nine done

--- lab2.js
+++ lab2.js
@@ -114,7 +114,9 @@ score +=1}
 */
 
 //CODE HERE
-
+let changeMyMind = false
+if (changeMyMind === true){changeMyMind=false, console.log(changeMyMind)}
+else if (changeMyMind === false){changeMyMind=true, console.log(changeMyMind)}
 
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
